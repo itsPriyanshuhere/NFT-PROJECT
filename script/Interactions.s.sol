416003b45371cd99bcd426b5e28163e3f55b1692
itsPriyanshuhere <priyanshupandey256@gmail.com>
2023-07-26 19:58:24 +0530

    Added few tests.

---
+++ script/Interactions.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+// contract MintBasicNft is Script{
+//     function run() external{
+//         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment();
+//     }
+// }
